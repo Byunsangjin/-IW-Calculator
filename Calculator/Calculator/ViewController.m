1171ed35ce481216e3ad158187ad4b50433f3883
Byunsangjin <qustkdwls1@naver.com>
2019-06-24 18:27:38 +0900

    버튼 제스쳐 처리

--- Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/ViewController.m
@@ -10,13 +10,43 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIView *plusView;
+
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *functionButtons;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *numberButtons;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    for (UIView *buttonView in self.functionButtons) {
+        UILongPressGestureRecognizer *tap = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(touchView:)];
+        [tap setMinimumPressDuration:0.05];
+        [buttonView addGestureRecognizer:tap];
+    }
+    
+    for (UIView *buttonView in self.numberButtons) {
+        UILongPressGestureRecognizer *tap = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(touchView:)];
+        [tap setMinimumPressDuration:0.05];
+        [buttonView addGestureRecognizer:tap];
+    }
+}
+
+- (void)touchView: (UILongPressGestureRecognizer *)gesture {
+    UIColor *originColor = gesture.view.backgroundColor;
+    UIColor *opacityColor;
+    
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        opacityColor = [originColor colorWithAlphaComponent:0.3];
+        [gesture.view setBackgroundColor:opacityColor];
+    } else if (gesture.state == UIGestureRecognizerStateEnded) {
+        opacityColor = [originColor colorWithAlphaComponent:1];
+        [gesture.view setBackgroundColor:opacityColor];
+    }
 }
 
 
